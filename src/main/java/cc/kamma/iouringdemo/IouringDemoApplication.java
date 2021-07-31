61a71d61b63ee5683f6b260d3aa67fe1ce30a408
Ding Guangyu <boaiwinserkua@gmail.com>
2021-07-31 22:17:44 +0800

    init

--- src/main/java/cc/kamma/iouringdemo/IouringDemoApplication.java
+++ src/main/java/cc/kamma/iouringdemo/IouringDemoApplication.java
@@ -1,13 +1,17 @@
 package cc.kamma.iouringdemo;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.EmptyByteBuf;
 import io.netty.channel.*;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.*;
 import io.netty.incubator.channel.uring.IOUringEventLoopGroup;
 import io.netty.incubator.channel.uring.IOUringServerSocketChannel;
+import io.netty.incubator.channel.uring.IOUringSocketChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
 
 @SpringBootApplication
 public class IouringDemoApplication {
@@ -20,12 +24,21 @@ public class IouringDemoApplication {
             ServerBootstrap b = new ServerBootstrap(); // (2)
             b.group(bossGroup, workerGroup)
                     .channel(IOUringServerSocketChannel.class) // (3)
-                    .childHandler(new ChannelInitializer<IOUringServerSocketChannel>() { // (4)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
-                        public void initChannel(IOUringServerSocketChannel ch) throws Exception {
+                        public void initChannel(SocketChannel ch) throws Exception {
                             final ChannelPipeline p = ch.pipeline();
-                            p.addLast(new HttpRequestDecoder());
-                            p.addLast(new HttpResponseEncoder());
+                            p.addLast("decoder", new HttpRequestDecoder());
+                            p.addLast("encoder", new HttpResponseEncoder());
+                            p.addLast("aggregator", new HttpObjectAggregator(1048576));
+                            p.addLast("handler", new SimpleChannelInboundHandler<FullHttpMessage>() {
+                                @Override
+                                protected void channelRead0(ChannelHandlerContext ctx, FullHttpMessage msg) throws Exception {
+                                    DefaultFullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+                                    httpResponse.headers().set(CONTENT_LENGTH, 0);
+                                    ctx.writeAndFlush(httpResponse);
+                                }
+                            });
                         }
                     })
                     .option(ChannelOption.SO_BACKLOG, 128)          // (5)
